b9621d0e4717701b05d1a6fcc75095552bad91a0
root <root@gpu005.rcp.epfl.ch>
2024-04-01 17:03:10 +0200

    infer sh

--- src/inference/infer.sh
+++ src/inference/infer.sh
@@ -8,5 +8,27 @@ if [ "$1" == "meditron-7b" ] || [ "$1" == "all" ]; then
         --output_path /home/Paul/make-discharge-me/data/infered/bhc_short_infered.csv.gz \
         --num_samples 100 \
         --verbose 1
+fi
+
+
+if [ "$1" == "medischarge-7b-hbc" ] || [ "$1" == "all" ]; then
+    python3 src/inference/infer.py \
+        --model_name medischarge-7b-hbc \
+        --model_path /pure-mlo-scratch/make_project/spring2024/trial-runs/meditron-7B-BHC \
+        --input_path data/preprocessed/test/bhc_short.csv.gz \
+        --prompt /home/Paul/make-discharge-me/src/prompts/bhc_test_pompt.json \
+        --output_path /home/Paul/make-discharge-me/data/infered/bhc_short_7b_infered.csv.gz \
+        --num_samples 10 \
+        --verbose 1
 fi     
 
+if [ "$1" == "medischarge-7b-di" ] || [ "$1" == "all" ]; then
+    python3 src/inference/infer.py \
+        --model_name medischarge-7b-di \
+        --model_path /pure-mlo-scratch/make_project/spring2024/trial-runs/meditron-7B-DI \
+        --input_path data/preprocessed/test/bhc_short.csv.gz \
+        --prompt /home/Paul/make-discharge-me/src/prompts/bhc_test_pompt.json \
+        --output_path /home/Paul/make-discharge-me/data/infered/di_short_7b_infered.csv.gz \
+        --num_samples 10 \
+        --verbose 1
+fi 
