ef280c87e901fbece9288cfd7c3078e937f96349
paulblgr <61545982+paulblgr@users.noreply.github.com>
2024-04-10 10:58:05 +0200

    infer openai ready

--- src/inference/infer_openai.py
+++ src/inference/infer_openai.py
@@ -9,6 +9,7 @@ import tiktoken
 import asyncio
 import nest_asyncio
 import pandas as pd
+from src.utils.loading_saving import load_file, save_file
 
 #from utils.prompts import advice_prompt, student_prompt, rec_prompt, rec_prompt_fast
 
@@ -109,21 +110,21 @@ class GPTWrapper():
         '''
         return (nb_tokens * self.cost_per_k_tokens) / 1000
     
-    def new_batch(self, msg, msg_len):
+    def new_batch(self, idx ,msg, msg_len):
         ''' Create a new batch. '''
-        return {"batch": pd.Series([msg]), "total_nb_token": msg_len}
+        return {"batch": pd.DataFrame({'idx': [idx], 'messages': [msg]}), "total_nb_token": msg_len}
 
     def partition(self, all_messages):
         ''' Build most optimal partitions given max number of tokens, model and messages.
         arguments:
-            - all_messages: pandas_series containing all messages to partition
+            - all_messages: pandas_df containing all messages to partition (idx, messages)
         returns:
-            - list of dictionary ("batch": batch of messages as a pandas serie, 
+            - list of dictionary ("batch": batch of messages as a pandas df (idx, message), 
                                 "total_nb_token": total number of tokens in the batch)
         '''
 
         batches = []
-        for _,message in tqdm(all_messages.items(), total=len(all_messages), desc="Partitioning messages"):
+        for _,idx,message in tqdm(all_messages.itertuples(), total=len(all_messages), desc="Partitioning messages"):
             nb_batches = len(batches)
             msg_len = self.count_tokens(message)
 
@@ -132,10 +133,10 @@ class GPTWrapper():
             else:
                 current_partition = batches[-1]
                 if current_partition["total_nb_token"] + msg_len <= self.max_tokens_per_batch:
-                    current_partition["batch"] = pd.concat([current_partition["batch"], pd.Series([message])])
+                    current_partition["batch"] = pd.concat([current_partition["batch"], pd.DataFrame({'idx': [idx], 'messages': [message]})])
                     current_partition["total_nb_token"] += msg_len
                 else:
-                    batches.append(self.new_batch(message, msg_len))
+                    batches.append(self.new_batch(idx, message, msg_len))
         
         return batches
 
@@ -168,7 +169,10 @@ class GPTWrapper():
         return responses_called
   
     def build_messages(self, user_prompt, sys_prompt=None):
-        ''' Build messages in the right format given system prompt and user prompt. '''
+        ''' Build messages in the right format given system prompt and user prompt.
+        Arguments:
+            - user_prompt: user prompt (str)
+            - sys_prompt: system prompt (str)'''
         messages = []
         if sys_prompt:
             messages.append({"role": "system", "content": sys_prompt})
@@ -210,36 +214,45 @@ class GPTWrapper():
             return response
         return response
 
-    def generate_answers(self, user_prompts, sys_prompt=None):
+    def generate_answers(self, user_prompts, save_path, sys_prompt=None):
         ''' Generate answers given a user prompt. 
         Arguments:
-            - user_prompts: list of user prompts (list(str)), will be splitted in batches
+            - user_prompts: pd dataframe of user prompts idx,prompt (int,str), will be splitted in batches
             - sys_prompt: system prompt (str)
+            - save_path: path to store the answers (str)
         Returns:
-            - list of answers (list(str))
+            - answers_df : dataframe of answers idx,prompt,answer (int,str,str)
         '''
 
-        #
-        '''We may wanna implement restart here, in case of failure of the API.'''
-        #
-
-        answers = []
+        #Load what was already done
+        if os.path.exists(save_path):
+            answers = load_file(save_path)
+            print(f"Loaded {answers.shape[0]} answers from {save_path}. Resuming generation.")
+        else:
+            answers = pd.DataFrame(columns=["idx", "prompt", "answer"])
         
-
-        all_messages = pd.Series(user_prompts).apply(lambda x: self.build_messages(x, sys_prompt))
-        batches = self.partition(all_messages)
+        #remove whats already done
+        done_idx = answers["idx"].tolist()
+        user_prompts = user_prompts[~user_prompts["idx"].isin(done_idx)].reset_index(drop=True)
+        
+        user_prompts['messages'] = user_prompts['prompt'].apply(lambda x: self.build_messages(x, sys_prompt))
+        batches = self.partition(user_prompts['idx', 'messages'])
 
         for i, batch_ in enumerate(batches):
-            batch = batch_["batch"]
+            batch = batch_["batch"]['messages']
+            idxs = batch_["batch"]['idx']
             nb_tokens = batch_["total_nb_token"]
-            print(f"Batch {i+1}/{len(batches)}: {batch.shape[0]} calls, {nb_tokens} total tokens, estimated cost: {self.estimate_cost(nb_tokens)}$")
+            print(f"Batch {i+1}/{len(batches)}: {batch.shape[0]} calls, {nb_tokens} total tokens, estimated input cost: {self.estimate_cost(nb_tokens)}$")
             loop = asyncio.get_event_loop()
             nest_asyncio.apply()
             start_time = time.time()
             current_answers = loop.run_until_complete(self.dispatch_openai_requests(batch))
-            answers.extend(current_answers)
-            
-            #some saving stuf here for the restart
+
+            current_answers = pd.DataFrame({'idx': idxs,'answer': current_answers})
+            current_answers = current_answers.merge(user_prompts[['idx', 'prompt']], on='idx', how='left')
+            current_answers.drop('messages', axis=1, inplace=True)
+            answers = pd.concat([answers, current_answers], axis=0)
+            save_file(answers, save_path)
 
             time_to_wait = max(5, 60 - (time.time() - start_time))
             print(f"\nWaiting {time_to_wait} seconds before next batch.")
@@ -249,137 +262,32 @@ class GPTWrapper():
         return answers
             
 
-class RecExtractor(GPTWrapper):
-    def __init__(self, model=MODEL, key_path=KEY_PATH):
-        super().__init__(model, key_path)
-        self.prompt = rec_prompt_fast
-        self.mode = 'rec_extraction_fast'
-
-    def filter_guidelines(self, source_path, save_path):
-        ''' Filter guidelines to remove already extracted guidelines. '''
-        guidelines = self.load(source_path)
-        print(f'Loaded {len(guidelines)} guidelines.')
-        generated = self.load(save_path)
-        generated_ids = set([g['id'] + '-' + 'q_type' for g in generated])
-        if len(generated) > 0:
-            print(f'Already extracted {len(generated_ids)} guidelines.')
-        guidelines = [g for g in guidelines if g['id'] + '-' + self.mode not in generated_ids]
-        return guidelines
-
-    def generate(self, source_path, save_path, verbose=False):
-        '''
-        Extract a list of clinical recommendations
-        from clinical practice guidelines.
-        '''
-        guidelines = self.filter_guidelines(source_path, save_path)
-
-        for guid_idx, guideline in tqdm(enumerate(guidelines), desc=f'Current cost: {self.cost}'):
-            user_prompt = self.prompt['user_prompt'].render(text=guideline['text'])
-            if verbose: print(user_prompt)
-            response = self.ask(user_prompt, sys_prompt=self.prompt['sys_prompt'], history=None)
-            if verbose: print(response)
-            recs = self.extract_recs(response)
-            print(f'Extracted {len(recs)} recommendations.')
-            for rec in recs:
-                record = {
-                    'id': guideline['id'],
-                    'source': guideline['source'],
-                    'q_type': self.mode,
-                    'text': rec
-                }
-                self.save(save_path, record)
-
-            if guid_idx > 5: 
-                break
-    
-    async def async_generate(self, source_path, save_path, verbose=False):
-        '''
-        Extract a list of clinical recommendations
-        from clinical practice guidelines.
-        '''
-        guidelines = self.filter_guidelines(source_path, save_path)
-
-        # partition guidelines by batches
-        partitions = self.partition(pd.DataFrame(guidelines), self.max_tokens)
-        print(f'Partitioned guidelines into {len(partitions)} batches.')
-        # [ [batch1], ..., [batch n] ]
-
-        async def one_call(guideline): # 1 guideline --> 1 call
-            ''' One async call to ask_chat. '''
-            user_prompt = self.prompt['user_prompt'].render(text=guideline['text'])
-            response = await self.async_ask(user_prompt, sys_prompt=self.prompt['sys_prompt'], history=None)
-            recs = self.extract_recs(response)
-            print(f'Extracted {len(recs)} recommendations.')
-            for rec in recs:
-                record = {
-                    'id': guideline['id'],
-                    'source': guideline['source'],
-                    'q_type': self.mode,
-                    'text': rec
-                }
-                self.save(save_path, record)
-            return recs
-        
-
-        async_responses = [one_call(guideline) for guideline in guidelines] 
-        new_responses = await asyncio.gather(*async_responses)
-        return new_responses
-    
-
-    
-
-    def extract_recs(self, response): 
-        ''' 
-        Extract recommendations from a response.
-        '''
-        recs = []
-        try: 
-            recs = json.loads(response)
-        except Exception as e:
-            print(f'Error extracting recommendations: {e}')
-        return recs
-
-class PatientExtractor(GPTWrapper):
-    def __init__(self, model=MODEL, key_path=KEY_PATH):
-        super().__init__(model, key_path)
-        raise NotImplementedError('PatientExtractor not yet implemented.')
-    
-class StudentExtractor(GPTWrapper):
-    def __init__(self, model=MODEL, key_path=KEY_PATH):
-        super().__init__(model, key_path)
-        raise NotImplementedError('StudentExtractor not yet implemented.')
-
-
-def main():
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--mode",
-        type=str,
-        default='recommendation',
-        help="Mode for extraction. Options: 'recommendation', 'patient', 'student'. Defaults to 'recommendation'."
-    )
+        '--input_path', type=str, required=True, help='Path to the input data file.')
     parser.add_argument(
-        "--source_path",
-        type=str,
-        default='guidelines.jsonl',
-        help="Data for scraped guidelines. Defaults to 'guidelines.jsonl'.")
+        '--output_path', type=str, required=True, help='Path to the output data file.')
     parser.add_argument(
-        "--save_path",
-        type=str,
-        default='guidelines_QA.jsonl',
-        help="Path to store extracted QA samples. Defaults to 'guidelines_QA.jsonl'.")
-    parser.add_argument(
-        "--keys_path",
-        type=str,
-        default='keys.json',
-        help="Path to OpenAI API keys. Defaults to 'keys.json'.")
-    args = parser.parse_args()
-    
-    extractors = {
-        'recommendation': RecExtractor,
-        'patient': PatientExtractor,
-        'student': StudentExtractor
-    }
+        '--sys_prompt_path', type=str, default=None, help='Path to the system prompt file.')
 
-    extractor = extractors[args.mode](args.keys_path)
-    extractor.generate(args.source_path, args.save_path)
+
+    args = parser.parse_args()
+
+    #check paths
+    if not os.path.exists(args.input_path):
+        raise ValueError(f'Input file not found at {args.input_path}.')
+    if not os.path.exists(args.sys_prompt_path):
+        sys_prompt = None
+    else:
+        with open(args.sys_prompt_path, 'r') as f:
+            sys_prompt = f.read()
+    
+    #load data
+    input_data_df = load_file(args.input_path)
+
+    #initialize GPTWrapper
+    gpt = GPTWrapper()
+
+    #generate answers
+    answers_df = gpt.generate_answers(input_data_df, args.output_path, sys_prompt=sys_prompt)
